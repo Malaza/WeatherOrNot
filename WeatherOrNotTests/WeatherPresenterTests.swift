c0e8e5452ac6c3ebdbe6103300e0edfd215fa9f4
Mthokozisi Malaza <mthokozisi.malaza@absa.africa>
2023-09-02 00:25:34 +0200

    Adding test classes

--- WeatherOrNotTests/WeatherPresenterTests.swift
+++ WeatherOrNotTests/WeatherPresenterTests.swift
@@ -1,14 +1,13 @@
 //
-//  WeatherOrNotTests.swift
+//  WeatherPresenterTests.swift
 //  WeatherOrNotTests
 //
-//  Created by Mthokozisi Malaza on 2023/08/30.
+//  Created by Mthokozisi Malaza on 2023/09/02.
 //
 
 import XCTest
-@testable import WeatherOrNot
 
-final class WeatherOrNotTests: XCTestCase {
+final class WeatherPresenterTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
